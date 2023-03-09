35302d9258cb62ce39ea887e7be4b9d6a0ce4370
Илья <IlyaT123@yandex.ru>
2023-03-09 20:45:43 +0300

    /

--- src/test/java/config/Properties.java
+++ src/test/java/config/Properties.java
@@ -8,13 +8,13 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class Properties {
     public static void settingsSelenoid() {
         //настройки selenoid  clean test -Dselenide.remote=https://user1:1234@selenoid.autotests.cloud/wd/hub
-        Configuration.driverManagerEnabled = true;
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.browserCapabilities = capabilities;
-        Configuration.browserSize = "1920x1080";
+//        Configuration.driverManagerEnabled = true;
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.browserCapabilities = capabilities;
+//        Configuration.browserSize = "1920x1080";
     }
 
     public static void settingsJenkins() {
