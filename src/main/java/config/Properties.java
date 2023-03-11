21a85bd0995633769e797497ccba7953fc6e6c8c
Илья <IlyaT123@yandex.ru>
2023-03-11 18:13:57 +0300

    ref

--- src/main/java/config/Properties.java
+++ src/main/java/config/Properties.java
@@ -5,16 +5,16 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Properties {
     public static void settingsSelenoid() {
         //настройки selenoid  clean test -Dselenide.remote=https://user1:1234@selenoid.autotests.cloud/wd/hub
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.driverManagerEnabled = true;
         Configuration.browserCapabilities = new DesiredCapabilities();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setCapability("enableVNC",  true);
-        chromeOptions.setCapability("enableVideo", true);
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserCapabilities.setCapability("enableVNC",  true);
+        Configuration.browserCapabilities.setCapability("enableVideo", true);
         Configuration.browserSize = "1920x1080";
     }
 
